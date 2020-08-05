83b16ea9e0dc12080322fba0f4275898272803e6
Eddy Rogier <eddyrogier@gmail.com>
2020-08-05 16:36:34 +0200

    tableView configuration

--- TodoList/TodoList/ViewController.swift
+++ TodoList/TodoList/ViewController.swift
@@ -9,12 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
   }
+}
 
-
+extension ViewController: UITableViewDataSource {
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    1
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+    cell.textLabel?.text = "coucou"
+    return cell
+  }
 }
 
