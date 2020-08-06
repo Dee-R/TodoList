734bdb69fd18a51fbb765242d78a0a395db3f455
Eddy Rogier <eddyrogier@gmail.com>
2020-08-06 11:34:01 +0200

    Add Todo

--- TodoList/TodoList/DetailViewController.swift
+++ TodoList/TodoList/DetailViewController.swift
@@ -1,14 +1,14 @@
-//
 //  DetailViewController.swift
 //  TodoList
 //
 //  Created by Eddy R on 05/08/2020.
 //  Copyright © 2020 Eddy R. All rights reserved.
 //  BOSS know info
+
+
 import UIKit
-// Fonction principal + Delegate
-protocol TapSaveDelegate{
-  // actionTime + les info a passer à l'interner pour qu'il les manipule
+
+protocol TapSaveDelegate {
   func didTapForSave(title:String)
 }
 
@@ -16,12 +16,11 @@ class DetailViewController: UIViewController, UITextFieldDelegate {
   @IBOutlet private weak var titleTodo: UITextField!
   @IBOutlet private weak var buttonSave: UIButton!
   var delegateTapSave:TapSaveDelegate!
-  
+
   override func viewDidLoad() {
     super.viewDidLoad()
      titleTodo.delegate = self
   }
-  
   internal func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
     let userEnteredString = textField.text
     let newString = (userEnteredString! as NSString).replacingCharacters(in: range, with: string) as NSString
@@ -37,8 +36,8 @@ class DetailViewController: UIViewController, UITextFieldDelegate {
     buttonSave.isHidden = boolValue
   }
   @IBAction func actionSave(_ sender: Any) {
-//    navigationController?.popViewController(animated: true)
-    delegateTapSave.didTapForSave(title: titleTodo.text!)
+    navigationController?.popViewController(animated: true)
+    delegateTapSave.didTapForSave(title: "title")
     dismiss(animated: true, completion: nil)
   }
 }
--- TodoList/TodoList/ViewController.swift
+++ TodoList/TodoList/ViewController.swift
@@ -5,46 +5,51 @@
 //  Created by Eddy R on 05/08/2020.
 //  Copyright © 2020 Eddy R. All rights reserved.
 // inter acting with info
-
+ 
 import UIKit
 
-class ViewController: UIViewController {
-  
-  @IBOutlet weak var tableView: UITableView!
-  
-  var todoList = ["buy Food", "clean the room"]
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    tableView.dataSource = self //ViewController est la datasource de tableview
-  }
-  
-  
-  @IBAction func actionAddTodo(_ sender: Any) {
-    print("  💟 coco 💟")
-    let viewControllerDestination = storyboard?.instantiateViewController(withIdentifier: "DetailViewController") as! DetailViewController
-    viewControllerDestination.navigationController?.navigationBar.topItem?.title = "toto"
-    viewControllerDestination.delegateTapSave = self
-    
-    present(viewControllerDestination, animated: true, completion: nil)
+struct ModelT {
+  let title:String!
+//  let id:String!
+//  let uuid = NSUUID().uuidString
+  init(title:String) {
+//    self.id = uuid
+    self.title = title
   }
 }
 
-extension ViewController: UITableViewDataSource {
+class ViewController: UIViewController, UITableViewDataSource, TapSaveDelegate {
+  func didTapForSave(title: String) {
+    todoList.append(ModelT(title: title))
+    tableView.reloadData()
+  }
+  
+  @IBOutlet weak var tableView: UITableView!
+  
+  var todoList = [
+    ModelT(title: "One"),
+    ModelT(title: "Two"),
+    ModelT(title: "Three")
+  ]
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    tableView.dataSource = self
+  }
+  @IBAction func actionAdd(_ sender: Any) {
+    let vc = storyboard?.instantiateViewController(withIdentifier: "DetailViewController") as! DetailViewController
+    print(vc)
+    vc.delegateTapSave = self
+    present(vc, animated: true, completion: nil)
+  }
+  
   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
     return todoList.count
   }
   
   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-    cell.textLabel?.text = todoList[indexPath.row]
+    cell.textLabel?.text = todoList[indexPath.row].title
+    print("cell", cell)
     return cell
-  }
-}
-
-extension ViewController: TapSaveDelegate {
-  func didTapForSave(title:String) {
-    todoList.append(title)
-    tableView.reloadData()
-    print("didTapForSave")
   }
 }
