dfb6d01e2b4392884c923a7866af60d8eba4ad7b
Eddy Rogier <eddyrogier@gmail.com>
2020-08-07 15:25:16 +0200

    Prepare segue to save data

--- TodoList/TaskViewController.swift
+++ TodoList/TaskViewController.swift
@@ -7,28 +7,36 @@
 //
 
 import UIKit
+import os.log
 
 class TaskViewController: UIViewController {
   
   // MARK: - Properties
   @IBOutlet weak var nameTextField: UITextField!
   
+  var task: Task?
+  @IBOutlet weak var saveButton: UIBarButtonItem!
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    
     // Do any additional setup after loading the view.
   }
   
-  
-  /*
-   // MARK: - Navigation
-   
-   // In a storyboard-based application, you will often want to do a little preparation before navigation
-   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-   // Get the new view controller using segue.destination.
-   // Pass the selected object to the new view controller.
-   }
-   */
-  
+  // MARK: - Navigation
+  // Configure the destination view controller only whe the save button is pressed.
+  override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    super.prepare(for: segue, sender: sender)
+    // verify is sender is a button (sender as? UIBarButtonItem)
+    // check if the object referenced is same with saveButton object (button === saveButton)
+    guard let button = sender as? UIBarButtonItem, button === saveButton else {
+      os_log("the save button was not pressed, cancelling", log: OSLog.default, type: .debug)
+      return
+    }
+    
+    // nil coalescing value is used to return the value of an optional if it is nil
+    let name = nameTextField.text ?? ""
+    
+    // set the task to be passed to ViewController after the unwind segue.
+    task = Task(name: name)
+  }
 }
